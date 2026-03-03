6e04a6aa67c2c8d95fb444146adcba2a48df3205
pxl1778 <peterlockhart72@gmail.com>
2026-03-02 21:04:04 -0800

    Add chapter name data

--- Code/DataScripts/NodeGraph/DialogueContainer.cs
+++ Code/DataScripts/NodeGraph/DialogueContainer.cs
@@ -8,6 +8,7 @@ public class DialogueContainer : ScriptableObject {
     [field: SerializeField] public string Id { get; private set; }
     [field: SerializeField] public string LoadPath { get; private set; }
     [field: SerializeField] public Sprite Thumbnail { get; set; }
+    [field: SerializeField] public string ChapterName { get; set; }
 
     public List<NodeLinkData> NodeLinks = new List<NodeLinkData>();
     public List<DialogueNodeData> DialogueNodeDatas = new List<DialogueNodeData>();
